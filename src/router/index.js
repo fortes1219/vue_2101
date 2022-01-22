fd518c8fd5075097e416a249d2cd1fe8ebf74116
AstolfoProto <tokiya.kagami@gmail.com>
2022-01-22 16:48:47 +0800

    add login example

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,67 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Cookies from 'js-cookie'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/home',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/Home.vue'),
+    meta: { requireAuth: true }
   },
   {
     path: '/restful_api',
     name: 'restful_api',
-    component: () => import('../views/RestfulApi.vue')
+    component: () => import('../views/RestfulApi.vue'),
+    meta: { requireAuth: true }
   },
   {
     path: '/vuex',
     name: 'vuex',
-    component: () => import('../views/Vuex.vue')
+    component: () => import('../views/Vuex.vue'),
+    meta: { requireAuth: true }
   },
   {
     path: '/form',
     name: 'form',
-    component: () => import('../views/form.vue')
+    component: () => import('../views/form.vue'),
+    meta: { requireAuth: true }
   },
   {
     path: '/i18n',
     name: 'i18n',
-    component: () => import('../views/i18n.vue')
+    component: () => import('../views/i18n.vue'),
+    meta: { requireAuth: true }
   },
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach(async(to, from, next) => {
+  if (to.meta.requireAuth) {
+    const userInfo = Cookies.get('login')
+    if (userInfo) {
+      const token = JSON.parse(userInfo).token
+      if (token.length > 0) {
+        next()
+      } else {
+        next({ name: 'Login' })
+      }
+    } else {
+      next({ name: 'Login' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row horizontal v_center" data-inset="1rem">
+    <div class="row vertical" data-width="20rem">
+      <el-input v-model="loginForm.username" type="text" placeholder="Username" />
+      <el-input v-model="loginForm.password" type="password" placeholder="Password" />
+      <br>
+      <el-button type="primary" @click="handleLogin">Login</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cookies from 'js-cookie'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: '',
+        token: 'asds32adsavrAS3Fadf5567'
+      }
+    }
+  },
+  methods: {
+    handleLogin() {
+      const user = this.loginForm.username
+      const pwd = this.loginForm.password
+      const token = this.loginForm.token
+      if (user !== '' && pwd !== '') {
+        Cookies.set('login', JSON.stringify(this.loginForm), { expires: 1 })
+      } else {
+        alert('帳號密碼不能為空')
+      }
+
+      if (Cookies.get('login') && this.loginForm.token) {
+        this.$router.push({ name: 'Home' })
+      }
+    },
+
+    removeCookie() {
+      Cookies.remove('login')
+    }
+  },
+  created() {
+    
+  },
+  mounted() {
+    this.removeCookie()
+  }
+}
+</script>
